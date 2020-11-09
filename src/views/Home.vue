27d73376b2666794ca727859eafb3f4e9924b206
Aisha Keita <aishakeita6@gmail.com>
2020-11-09 08:54:34 +0100

    updated each cel in its own grid component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,83 +1,148 @@
 // "HTML"
 <template>
   <div class="home">
-    <div class="container">
-      <div class="row">
-        <div class="col-sm"></div>
-        <div class="col-sm"></div>
-        <div class="col-sm"></div>
-      </div>
-      <div class="row">
-        <div class="col-sm"></div>
-        <div class="col-sm"></div>
-        <div class="col-sm"></div>
-      </div>
-      <div class="row">
-        <div class="col-sm"></div>
-        <div class="col-sm"></div>
-        <div class="col-sm"></div>
-      </div>
+    <div class="checkWin"></div>
+    <div class="checkDraw"></div>
+    <v-container class="grey lighten-5">
+      <v-row no-gutters>
+        <v-col v-for="n in 3" :key="n">
+            <Square 
+            v-bind:num="board[0][n-1].num"
+            v-bind:value="board[0][n-1].value"/>
+        </v-col>
+      </v-row>
+      <v-row no-gutters>
+        <v-col v-for="n in 3" :key="n">
+            <Square 
+            v-bind:num="board[1][n-1].num"
+            v-bind:value="board[1][n-1].value"/>
+        </v-col>
+      </v-row>
+      <v-row no-gutters>
+        <v-col v-for="n in 3" :key="n">
+            <Square 
+            v-bind:num="board[2][n-1].num"
+            v-bind:value="board[2][n-1].value"/>
+        </v-col>
+      </v-row>
+    </v-container>
+    <div class="text-sm-center">
+      <v-btn color="primary">RESTART GAME</v-btn>
     </div>
-    <div class="col -md- 12 text-center">
-      <button type="button" class="btn btn-primary btn-lg">RESTART GAME</button>
-    </div>
-    <!-- <div><v-btn depressed color="primary"> RESTART GAME</v-btn></div> -->
   </div>
 </template>
 
 // Javascript
 <script>
+import Square from "./Square.vue";
 export default {
   name: "Home",
+  components: {
+      Square: Square,
+  },
   data() {
     return {
       currentPlayer: "x", // is  between x and o
-      draw: "", // no win or after everything is click
+
       board: [
-        //an array of an array and each array is represing a row
-        ["1", "2", "3"], //the  values inside the array is representing a column
-        ["4", "5", "6"],
-        ["7", "8", "9"]
+        [   { num: 1, value: null },
+            { num: 2, value: null },
+            { num: 3, value: null }
+        ],   
+        [   { num: 4, value: null },
+            { num: 5, value: null },
+            { num: 6, value: null }
+        ],
+        [   { num: 7, value: null },
+            { num: 8, value: null },
+            { num: 9, value: null }
+        ]
       ],
       methods: {
-          //for loop 
+        //checkWin()
+        //checkDraw() {}
       }
     };
   },
   created() {
-    console.log(this.board[2][2]);
-    console.log(this.board[1][1]);
+    //wins
+    // console.log(this.board[0][0],this.board[0][1],this.board[0][2]);//123
+    // console.log(this.board[1][0],this.board[1][1],this.board[1][2]);//456
+    // console.log(this.board[2][0],this.board[2][1],this.board[2][2]);//789
+    // console.log(this.board[0][0],this.board[1][1],this.board[2][2]); //159
+    // console.log(this.board[2][2],this.board[1][1],this.board[0][0]);//951
+    // console.log(this.board[0][2],this.board[1][1],this.board[2][0]); //358
+    // console.log(this.board[2][0],this.board[1][1],this.board[0][2]);//758
+    // console.log(this.board[0][2],this.board[0][1],this.board[0][0]);//321
+    // console.log(this.board[1][2],this.board[1][1],this.board[1][0]);//654
+    // console.log(this.board[2][2],this.board[2][1],this.board[2][0]);//951
+    // if (this.currentPlayer === this.board[0][0] ||
+    //     this.board[0][1] || this.board[0][2]) {
+    // } else if (currentPlayer === this.board[1][0] ||
+    //   this.board[1][1] || this.board[1][2]
+    // ) {
+    //   return youWonMessag;
+    // } else if (
+    //   currentPlayer === this.board[2][0] ||
+    //   this.board[2][1] ||this.board[2][2]
+    // ) {
+    //   return youWonMessag;
+    // } else if (
+    //   currentPlayer === this.board[0][0] ||
+    //   this.board[1][1] ||this.board[2][2]
+    // ) {
+    //   return youWonMessag;
+    // } else if (this.board[0][2] || this.board[1][1] || this.board[2][0]) {
+    //   return youWonMessag;
+    // } else if (
+    //   currentPlayer === this.board[2][0] ||
+    //   this.board[1][1] ||
+    //   this.board[0][2]
+    // ) {
+    //   return youWonMessag;
+    // } else if (
+    //   currentPlayer === this.board[0][2] ||
+    //   this.board[0][1] ||
+    //   this.board[0][0]
+    // ) {
+    //   return youWonMessag;
+    // } else if (
+    //   currentPlayer === this.board[1][2] ||
+    //   this.board[1][1] ||
+    //   this.board[1][0]
+    // ) {
+    //   return youWonMessag;
+    // } else if (
+    //   currentPlayer === this.board[2][2] ||
+    //   this.board[2][1] ||
+    //   this.board[2][0]
+    // ) {
+    //   return youWonMessag;
+    // } else {
+    //   draw;
+    // }
   }
 };
 </script>
 
 // Styling
 <style lang="scss" scoped>
-.container,
-.row {
-  background-color: lightgray;
-}
+// .card-border {
+//   border: 3px solid black;
+//   border-top: none;
+//   border-left: none;
+//   //border-right: none;
+//   border-bottom: none;
+//   font-size: 30px;
+//   text-align: center;
+//   cursor: pointer;
+//   padding: 20px;
+// }
 
-.col-sm {
-  border: 5px solid black;
-  //border-top-style: none;
-  padding: 10%;
-  font-size: 30px;
-  text-align: center;
-  cursor: pointer;
-  //border-bottom-width: 0;
-}
-
-.home {
-  padding: 20px;
-  background-color: white;
-  height: 100vh;
-}
-
-.btn-primary,
-button {
-  background-color: #bde;
-  padding: 1%;
-  font-size: 34px;
-}
+// .red {
+//   border-bottom-style: none;
+// }
+// .text-sm-center {
+//   font-size: 54px;
+// }
 </style>
---
+++ src/views/Square.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="square">
+      <v-card @click="printNum" class="pa-2 square-cell" outlined tile>{{this.value}}</v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Square",
+    props: ["num"],
+  data() {
+    return{
+        value: null,
+    }
+   
+  },
+  methods: {
+      printNum() {
+           console.log(this.num);
+           console.log(this.value);
+           this.value = "O";
+      },
+  },
+  created(){
+      //console.log(this.num);
+  }
+
+};
+
+</script>
+<style lang="scss" scoped>
+</style>
