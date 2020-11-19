35e112edbb9ea41f4d40089710e67a8284ea7884
Aisha Keita <aishakeita6@gmail.com>
2020-11-19 13:22:08 +0100

    server: add a get a single square from the board Api

--- server/index.js
+++ server/index.js
@@ -14,28 +14,38 @@ app.use(express.urlencoded({ extended: true }));
 
 let board = [
     [
-      { id: 1, value: null },
-      { id: 2, value: null },
-      { id: 3, value: null }
+        { id: 1, value: null },
+        { id: 2, value: null },
+        { id: 3, value: null }
     ],
     [
-      { id: 4, value: null },
-      { id: 5, value: null },
-      { id: 6, value: null }
+        { id: 4, value: null },
+        { id: 5, value: null },
+        { id: 6, value: null }
     ],
     [
-      { id: 7, value: null },
-      { id: 8, value: null },
-      { id: 9, value: null }
+        { id: 7, value: null },
+        { id: 8, value: null },
+        { id: 9, value: null }
     ]
-  ];
+];
 
 //here you can do dfferent request: post, delete, put, patch etc.
-//basically for testing your backend on post man, web etc.
+//basically for testing your backend on post man, or the web etc.
+//for this line of code we are doing a get request method
+//board will be use to get to the endpoint
 app.get('/board', (req, res) => {
-    res.send(board);// defining the reuter and sending a result
-})
+    res.send(board);
+});
+
+//here you can get a single square from the board API a parameter of for example id :1 etc.
+app.get('/board/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const square = board.flat().find(ele => ele.id === id);
+    console.log(board[0][0]);
+    res.send(square);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
