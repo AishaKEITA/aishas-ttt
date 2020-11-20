76bff6997ad84a452667572d8a51c058ad62eb98
Aisha Keita <aishakeita6@gmail.com>
2020-11-20 10:54:47 +0100

    client: fetch api's from the backend and remove game logic from frontend

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,32 +1,56 @@
 
 <template>
-  <div v-if="board" class="home ">
+  <div
+    v-if="board"
+    class="home"
+  >
     //the render of 3x3 grid with square components
     <v-container class="grey lighten-5 square-container fluid ma-0 pa-0">
-      <v-row dense no-gutters>
-        <v-col width="100" v-for="n in 3" :key="n">
+      <v-row
+        dense
+        no-gutters
+      >
+        <v-col
+          v-for="n in 3"
+          :key="n"
+          width="100"
+        >
           <Square
-            v-on:square-click="squareClick"
-            v-bind:id="board[0][n - 1].id"
-            v-bind:value="board[0][n - 1].value"
+            :id="board[0][n - 1].id"
+            :value="board[0][n - 1].value"
+            @square-click="squareClick"
           />
         </v-col>
       </v-row>
-      <v-row dense no-gutters>
-        <v-col width="100" v-for="n in 3" :key="n">
+      <v-row
+        dense
+        no-gutters
+      >
+        <v-col
+          v-for="n in 3"
+          :key="n"
+          width="100"
+        >
           <Square
-            v-on:square-click="squareClick"
-            v-bind:id="board[1][n - 1].id"
-            v-bind:value="board[1][n - 1].value"
+            :id="board[1][n - 1].id"
+            :value="board[1][n - 1].value"
+            @square-click="squareClick"
           />
         </v-col>
       </v-row>
-      <v-row dense no-gutters>
-        <v-col width="100" v-for="n in 3" :key="n">
+      <v-row
+        dense
+        no-gutters
+      >
+        <v-col
+          v-for="n in 3"
+          :key="n"
+          width="100"
+        >
           <Square
-            v-on:square-click="squareClick"
-            v-bind:id="board[2][n - 1].id"
-            v-bind:value="board[2][n - 1].value"
+            :id="board[2][n - 1].id"
+            :value="board[2][n - 1].value"
+            @square-click="squareClick"
           />
         </v-col>
       </v-row>
@@ -37,17 +61,25 @@
       align="center"
       justify="center"
     >
-      <v-btn @click="resetGame" color="normal">RESTART GAME</v-btn>
+      <v-btn
+        color="normal"
+        @click="resetGame"
+      >
+        RESTART GAME
+      </v-btn>
     </div>
     <div class="text-center">
-      <v-dialog v-model="dialog" width="300">
+      <v-dialog
+        v-model="dialog"
+        width="300"
+      >
         <v-card>
           <v-card-title class="headline grey lighten-2">
             {{ gameOverText }}
           </v-card-title>
-          <v-divider></v-divider>
+          <v-divider />
           <v-card-actions>
-            <v-spacer></v-spacer>
+            <v-spacer />
             <v-btn
               color="primary"
               text
@@ -56,7 +88,9 @@
                 resetGame();
               "
             >
-              <div class="play-color">Play Again</div>
+              <div class="play-color">
+                Play Again
+              </div>
             </v-btn>
           </v-card-actions>
         </v-card>
@@ -67,128 +101,106 @@
 <script>
 import Square from "./Square.vue";
 export default {
-  name: "Home",
-  components: {
+    name: "Home",
     //component for the square
-    Square: Square
-  },
-
-  data() {
-    return {
-      currentPlayer: "X",
-      dialog: false,
-      gameOverText: "",
-      counter: 0,
-
-      //array for the board number for each square
-      board: null,
-    };
-  },
-  created() {
-   this.fetchBoard();
-  },
-  methods: {
-   fetchBoard() {
-      fetch("http://localhost:3000/board")
-      .then(response => response.json())
-      .then(data => {
-        this.board = data;
-      });
-    },
-    //this function contains all the possible win conditions
-    checkWin() {
-      //checking horizontal
-      for (let i = 0; i < this.board.length; i++) {
-        if (
-          this.board[i][0].value === this.currentPlayer &&
-          this.board[i][0].value === this.board[i][1].value &&
-          this.board[i][1].value === this.board[i][2].value
-        ) {
-          return true;
-        }
-      }
-
-      //checking vertical
-      for (let i = 0; i < this.board.length; i++) {
-        if (
-          this.board[0][i].value === this.currentPlayer &&
-          this.board[0][i].value === this.board[1][i].value &&
-          this.board[1][i].value === this.board[2][i].value
-        ) {
-          return true;
-        }
-      }
-
-      //checking diagonal
-      if (
-        this.board[0][0].value === this.currentPlayer &&
-        this.board[0][0].value === this.board[1][1].value &&
-        this.board[1][1].value === this.board[2][2].value
-      ) {
-        return true;
-      }
-      if (
-        this.board[2][0].value === this.currentPlayer &&
-        this.board[2][0].value === this.board[1][1].value &&
-        this.board[1][1].value === this.board[0][2].value
-      ) {
-        return true;
-      }
-
-      return false;
+    components: {
+        Square: Square
     },
 
-    //this function checks when a square is clicked. The squareId is a parameter
-    squareClick(squareId) {
-      let gameOver = false;
-      const clickedSquare = this.board
-        .flat()
-        .find(square => square.id === squareId);
+    data() {
+        return {
+            currentPlayer: "X",
+            dialog: false,
+            gameOverText: "",
+            counter: 0,
 
-      if (clickedSquare.value != null) {
-        return;
-      }
-      clickedSquare.value = this.currentPlayer;
-
-      //this is the gameover function
-      gameOver = this.checkWin();
-
-      //this if statement checks if won the game is won
-      if (gameOver) {
-        this.gameOverText = "Player: " + this.currentPlayer + " Won!";
-        this.dialog = true;
-      }
-      this.counter++;
-
-      //this if statement checks if the game is draw
-      if (!gameOver && this.counter >= 9) {
-        this.gameOverText = "IT'S A DRAW!!!";
-        this.dialog = true;
-        gameOver = true;
-      }
-
-      //this if statement checks to alternate between player x and player o
-      if (this.currentPlayer === "X") {
-        this.currentPlayer = "O";
-      } else {
-        this.currentPlayer = "X";
-      }
+            //array for the board number for each square
+            board: null
+        };
     },
+    //use created to call the fetchBoard function in method
+    created() {
+        this.fetchBoard();
+    },
+    methods: {
+        //created a fetchBoard function to fetch the board Api from the server and assigned it to the board variable in the template
+        fetchBoard() {
+            fetch("http://localhost:3000/board")
+                .then(response => response.json())
+                .then(data => {
+                    this.board = data;
+                    //TODO: only assign this.board if response.ok
+                });
+        },
+        checkGameOver(){
+            fetch("http://localhost:3000/board/checkGameOver")
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                    //TODO: only assign this.board if response.ok
+                });
+        },
+        //this function is calling the backend Api to ensure every click on the square is communicating to the server
+        setSquareBackend(squareId, newValue) {
+            fetch(`http://localhost:3000/board/id/${squareId}`, {
+                method: "PUT",
+                headers: { "Content-type": "application/json; charset=UTF-8" },
+                body: JSON.stringify({ value: newValue })
+            }).then(response => {
+                if (response.ok) {
+                    response.json().then(json => {
+                        const square = this.board
+                            .flat()
+                            .find(square => square.id === squareId);
+                        square.value = json.value;
 
-    //this function resets the game by using a for of loop to acess board
-    resetGame() {
-      for (let i = 0; i < this.board.length; i++) {
-        for (let j = 0; j < this.board[i].length; j++) {
-          this.board[i][j].value = null;
+                        //this if statement checks to alternate between player x and player o
+                        if (this.currentPlayer === "X") {
+                            this.currentPlayer = "O";
+                        } else {
+                            this.currentPlayer = "X";
+                        }
+                    })
+                }
+            })
+        },
+        //this function resets the game by using a for of loop to access board
+        resetGame() {
+            fetch("http://localhost:3000/board/reset", {
+                method: "POST"
+            }).then(response => {
+                if (response.ok) {
+                    response.json().then(json => {
+                        this.board = json;
+
+                        //this resets the current player x, counter, dialog to false, and game over text
+                        this.currentPlayer = "X";
+                        this.dialog = false;
+                        this.gameOverText = "";
+                    })
+                }
+            })
+        },
+        //this function checks when a square is clicked. The squareId is a parameter
+        squareClick(squareId) {
+            //call the api from the server here
+            this.setSquareBackend(squareId, this.currentPlayer);
+
+            //this is the gameover function
+            let gameOver = this.checkGameOver();
+
+            //TODO: checks the return from checkGameOver and sets gameOverText
+            if (gameOver) { //gameOver.gameover
+                //if gameOver.gameover == true, gameOver.winner != null
+                this.gameOverText = "Player: " + this.currentPlayer + " Won!";
+                this.dialog = true;
+                //else if (gameOver.gameover == true, gameOver.winner == null) {
+                //  this.gameOverText = "IT'S A DRAW!!!";
+                //  this.dialog = true;
+                //}
+            }
         }
-      }
-      //this resets the current player x, counter, dialog to false, and game over text
-      this.currentPlayer = "X";
-      this.counter = 0;
-      this.dialog = false;
-      this.gameOverText = "";
     }
-  }
 };
 </script>
 
