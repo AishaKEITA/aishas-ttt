17bca1dd5663281c1bd372fefda365f70f9101ed
Aisha Keita <aishakeita6@gmail.com>
2020-12-02 18:15:40 +0100

    server: Single dimensional array and function
    - change board to a single dimensional array
    - change the checkwin function
    - modify the app put Api
    - modify the app get Api
    - modify the app post Api

--- server/index.js
+++ server/index.js
@@ -11,53 +11,47 @@ app.use(cors());
 let lastPlayer = null;
 let counter = 0;
 
+//using a single dimensional array for the game board
 let board = [
-    [
-        { id: 1, value: null },
-        { id: 2, value: null },
-        { id: 3, value: null }
-    ],
-    [
-        { id: 4, value: null },
-        { id: 5, value: null },
-        { id: 6, value: null }
-    ],
-    [
-        { id: 7, value: null },
-        { id: 8, value: null },
-        { id: 9, value: null }
-    ]
-];
+    "", "", "",
+    "", "", "",
+    "", "", "",
+]
 
+// logical changes. each change should be done in server + client. verify. commit.
+//1. change existing board array to one dimensional array
+//2. change from value from null to empty string
+//3. remove id
+//4. change square from { value: "" } to ""
+
+//utility function
 function checkForEqual(value1, value2, value3) {
-    return value1 != null &&
+    return value1 != "" &&
         value1 === value2 &&
         value2 === value3;
 }
-
 function checkWin() {
-    //checking horizontal
-    for (let i = 0; i < board.length; i++) {
-        if (checkForEqual(board[i][0].value, board[i][1].value, board[i][2].value)) {
-            return { gameover: true, winner: board[i][0].value };
+    const rows = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+
+    // Iterate over array with winning combinations
+    for (let i = 0; i < rows.length; i++) {
+        const [a, b, c] = rows[i]
+        // Check if the game board contains winning combination
+        if (checkForEqual(board[a] && board[a] === board[b] && board[a] === board[c])) {
+            // Return the winner ('x' or 'o')
+            return { gameover: true, winner: board[a] };
         }
     }
-
-    //checking vertical
-    for (let i = 0; i < board.length; i++) {
-        if (checkForEqual(board[0][i].value, board[1][i].value, board[2][i].value)) {
-            return { gameover: true, winner: board[0][i].value };
-        }
-    }
-
-    //checking diagonal
-    if (checkForEqual(board[0][0].value, board[1][1].value, board[2][2].value)) {
-        return { gameover: true, winner: board[0][0].value };
-    }
-    if (checkForEqual(board[2][0].value, board[1][1].value, board[0][2].value)) {
-        return { gameover: true, winner: board[2][0].value };
-    }
-
+    // Otherwise do nothing
     return { gameover: false, winner: null };
 }
 
@@ -82,12 +76,14 @@ function checkGameOver() {
 //basically for testing your backend on post man, or the web.
 //for this line of code we are doing a get request method
 //board will be use to get to the endpoint
+// Simply get Board API. Gets a list of all the Squares
 app.get('/board', (req, res) => {
     res.send(board);
 });
 //put update data in the server
 //here you can set square to see what is clicked on the client
 //req.params request a parameter of an id
+// Set Square API. Set the value of a single Square
 app.put('/board/id/:id', (req, res) => {
     //if the game is over, return error
     let gameOverObject = checkGameOver();
@@ -97,8 +93,7 @@ app.put('/board/id/:id', (req, res) => {
 
     //check if square is not found, then return HTTP error
     const id = Number(req.params.id);
-    const square = board.flat().find(ele => ele.id === id);
-    if (!square) {
+    if (id < 0 || id >= board.length) {
         return res.status(400).send({error:'invalid square id'});
     }
 
@@ -112,8 +107,13 @@ app.put('/board/id/:id', (req, res) => {
         return res.status(400).send({error: 'value has to be X or O'});
     }
 
+    //check that the first move in a new game is 'X', else return HTTP error
+    if (counter === 0 && req.body.value !== "X") {
+        return res.status(400).send({error: 'first move has to be X'});
+    }
+
     //here we check if square is not clicked by accessing the square object property
-    if (square.value != null) {
+    if (board[id] != "") {
         return res.status(400).send({error: 'square already has a value'});
     }
 
@@ -122,37 +122,32 @@ app.put('/board/id/:id', (req, res) => {
         return res.status(400).send({error: `it is not player ${lastPlayer}'s turn`});
     }
 
-    square.value = req.body.value;
-    lastPlayer = square.value;
+    board[id] = req.body.value;
+    lastPlayer = board[id];
     counter++;
 
-    res.send(square);
+    res.send(board[id]);
 });
-//here you can get a single square from the board Api a parameter of for example, id :1 etc.
+// Get Square API. Gets the value of a single Square for example, id/1 etc
 app.get('/board/id/:id', (req, res) => {
-    const id = Number(req.params.id);
-    const square = board.flat().find(ele => ele.id === id);
-
     //check if square is not found, then return HTTP error
-    if (!square) {
+    const id = Number(req.params.id);
+    if (id < 0 || id >= board.length) {
         return res.status(400).send({error:'invalid square id'});
     }
 
-    res.send(square);
+    res.send(board[id]);
 });
 //this is a post request for the reset of the board
 app.post('/board/reset', (req, res) => {
     for (let i = 0; i < board.length; i++) {
-        for (let j = 0; j < board[i].length; j++) {
-            board[i][j].value = null;
-        }
+        board[i] = "";
     }
     lastPlayer = null;
     counter = 0;
 
     res.send(board);
 });
-
 //checks if there is a win or a draw
 app.get('/board/checkGameOver', (req, res) => {
     let gameOverObject = checkGameOver();
